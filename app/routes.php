e271bb0ea7a59c2bade3153de6257187ecb88dbd
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-08-06 16:50:52 +0300

    Store message in database

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,26 @@
 <?php
 
-$app->get('/home', function($request, $response, $args) {
-
-	var_dump($this->db->query('SELECT 1')->fetch(PDO::FETCH_OBJ));
-
+$app->get('/', function($request, $response, $args) {
     $this->view->render($response, 'home.twig');
-});
+})->setName('home');
+
+$app->post('/post', function($request, $response, $args) use ($app) {
+	// Get parameters
+	$params = $request->getParams();
+	// Generate hash
+	$hash = md5(uniqid(true));
+
+	$message = $this->db->prepare("
+		INSERT INTO messages(hash, body) 
+		VALUES (:hash, :body)
+	");
+
+	$message->execute([
+		'hash'    => $hash,
+		'body' => $params['body'],
+	]);
+
+	// TODO: Use Slim to redirect
+	return $response->withRedirect('/');
+
+})->setName('send');
